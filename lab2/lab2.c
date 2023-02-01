8ffb8543d4884ce9ae2a82b38f4309ed17a9efc3
Hyunwoo Choi <hyunwoo6@illinois.edu>
2023-01-31 19:43:24 -0600

    finished lab2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -3,13 +3,11 @@
 
 int main()  
 {  
-    int a;
-
-    for (a = 256; a > 1; a = sqrt(a))  
-    {  
-        printf("%d\n", a);  
-    }  
-
+    int a = 256;
+    while (a > 1){
+	printf("%d\n", a);
+	a = sqrt(a);
+    }			
     printf("%d\n", 1);  
 
     return 0;
