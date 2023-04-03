7cdd1cf4d3597b88a7a581db62ac59137958ac12
Hyunwoo Choi <hyunwoo6@illinois.edu>
2023-04-02 23:15:58 -0500

    lab9 completed

---
+++ first-program/hello-world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#define MESSAGE "Hello, x86 world!\n"
+int main(){
+	printf(MESSAGE);
+	return 0;
+}
---
+++ lab2/num_comps.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main()
+{
+    float  val1 = 10;
+    float val2 = 3;
+    float res;
+ 
+    res = val1 / val2;
+ 
+    printf("%f\n", res);
+ 
+    return 0;
+}
